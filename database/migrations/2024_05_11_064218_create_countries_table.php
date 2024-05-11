2b2b8da878ad0824ddf965eafce20d0bb73018a0
dkAsCambodia <nitindamo50@gmail.com>
2024-05-11 18:10:34 +0700

    add one table for user current location

---
+++ database/migrations/2024_05_11_064218_create_countries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('country_name')->nullable();
+            $table->string('country_code')->nullable();
+            $table->string('mobile_country_code')->nullable();
+            $table->string('currency_name')->nullable();
+            $table->string('curency_code')->nullable();
+            $table->string('currency_symbol')->nullable();
+            $table->string('order')->nullable();
+            $table->string('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
